b86942dac61a59be43bdb599608da8dacf7c5e27
abhigit-design <asomani2612@gmail.com>
2025-11-04 16:17:55 -0500

    Update data source path in load_data.sql

--- scripts/load_data.sql
+++ scripts/load_data.sql
@@ -4,6 +4,6 @@ CREATE OR REPLACE FILE FORMAT my_csv_format
   SKIP_HEADER = 1;
 
 COPY INTO sample_sales
-FROM @~/sample_sales_data.csv
+FROM @MY_STAGE/sample_sales_data.csv
 FILE_FORMAT = (FORMAT_NAME = my_csv_format)
 ON_ERROR = 'CONTINUE';
